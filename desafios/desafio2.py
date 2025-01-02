b34efc28e090c0391f8090432899234bfccac987
Anguna <nathansantosanguna@gmail.com>
2025-01-02 19:08:13 -0300

    iniciando

---
+++ desafios/desafio2.py
@@ -0,0 +1,4 @@
+dia = input('Dia')
+mes = input('Mês')
+ano = input('Ano')
+print ('Você nasceu dia '+ dia +' no mes '+mes+' no ano de '+ano)
---
+++ desafios/desafio3.py
@@ -0,0 +1,3 @@
+numerum = input('Primeiro numero')
+numerdois = input('Segundo numero')
+print (numerum + numerdois)
