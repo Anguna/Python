f438a07ff9d7aa804c5f11e3c6c7a2aad5518309
Anguna <nathansantosanguna@gmail.com>
2025-01-10 20:00:44 -0300

    alguns exercicios

---
+++ desafios/ex011.py
@@ -0,0 +1,5 @@
+largura = float(input('Qual a largura da parede?'))
+altura = float(input('Qual a altura da parede?'))
+area = largura * altura
+res = area / 2
+print('Para pintar essa parede, voce precisara de {}l de tinta'.format(res))
