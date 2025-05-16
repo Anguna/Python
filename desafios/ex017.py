91bf4970a2b95292e3e652607f473b453f29afd7
Anguna <nathansantosanguna@gmail.com>
2025-05-16 15:57:55 -0300

    exercicio 17

---
+++ desafios/ex017.py
@@ -0,0 +1,4 @@
+cat1 = float(input('Compriento do cateto oposto:'))
+cat2 = float(input('Comprimento do cateto adjacente:'))
+hip = (cat1**2+cat2**2)**0.5
+print('a hipotenusa vai medir {:.2f}'.format(hip))
