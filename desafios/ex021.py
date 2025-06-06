725bb7d02d3a1b10d2fe8bd5861b01308531e065
Anguna <nathansantosanguna@gmail.com>
2025-06-06 15:19:27 -0300

    ex021

--- desafios/ex021.py
+++ desafios/ex021.py
@@ -1,6 +1,7 @@
-
 import pygame
-pygame.init()
+import time
+pygame.mixer.init()
 pygame.mixer.music.load('ex021.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
+pygame.mixer.music.play(loops=0)
+while pygame.mixer.music.get_busy()
+    time.sleep(1)
